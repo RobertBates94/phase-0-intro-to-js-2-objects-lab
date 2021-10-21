a13230d4be367dfea2ce54718c7ce0df7a1cf74f
Robert <bates.to.infinity@gmail.com>
2021-10-20 19:59:25 -0500

    completed assignment

--- index.js
+++ index.js
@@ -16,5 +16,12 @@ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
 }
 
 function deleteFromEmployeeByKey(employee, key) {
-        delete employee.key
+        const newEmployee = {...employee}
+        delete newEmployee[key]
+        return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
 }
